c650abe817e1fe87ef75fcdec017f352cae713f6
黄卫东 <1668369668@qq.com>
2024-09-25 16:55:50 +0800

    批号

---
+++ PigRunner.WebApi/PigRunner.Public/Common/Views/LotMasterRequest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PigRunner.Public.Common.Views
+{
+    public class LotMasterRequest
+    {
+        /// <summary>
+        /// 批号
+        /// </summary>
+        public string LotCode { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 料品
+        /// </summary>
+        public long ItemMaster { get; set; } = 0;
+
+        /// <summary>
+        /// 组织
+        /// </summary>
+        public long Org { get; set; } = 0;
+
+        /// <summary>
+        /// 生效日期
+        /// </summary>
+        public DateTime EffectiveDate { get; set; }
+
+        /// <summary>
+        /// 生效天数
+        /// </summary>
+        public int ValidDate { get; set; }
+
+        /// <summary>
+        /// 失效日期
+        /// </summary>
+        public DateTime InvalidDate { get; set; }
+
+        /// <summary>
+        /// 来源单号
+        /// </summary>
+        public string SrcDocNo { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 自动编码
+        /// </summary>
+        public long AutoCode { get; set; } = 0;
+
+
+        /// <summary>
+        /// 备注
+        /// </summary>
+        public string Memo { get; set; } = string.Empty;
+    }
+}
---
+++ PigRunner.WebApi/PigRunner.Public/Common/Views/PubResponse.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PigRunner.Public.Common.Views
+{
+    /// <summary>
+    /// 公共返回实体
+    /// </summary>
+    public class PubResponse
+    {
+        /// <summary>
+        /// 是否成功
+        /// </summary>
+        public bool Success { get; set; } = false;
+        /// <summary>
+        /// 信息
+        /// </summary>
+        public string Message { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 单号/编码
+        /// </summary>
+        public string Code { get; set; }= string.Empty;
+        
+
+        public static PubResponse Error(string msg)
+        {
+            return new PubResponse { Success = false, Message = msg };
+        }
+    }
+}
