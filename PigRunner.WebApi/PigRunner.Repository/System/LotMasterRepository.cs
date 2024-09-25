f61cdc475cca1865e9922362bf9d4364e9ba8bd3
黄卫东 <1668369668@qq.com>
2024-09-25 16:56:57 +0800

    批号

---
+++ PigRunner.WebApi/PigRunner.Repository/System/LotMasterRepository.cs
@@ -0,0 +1,38 @@
+﻿using PigRunner.Entitys.BarCode;
+using PigRunner.Public.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PigRunner.Repository.System
+{
+     /// <summary>
+     /// 批号存储库
+     /// </summary>
+    public class LotMasterRepository : BaseRepository<BcLotMaster>, IScopedService
+    {
+        
+        /// <summary>
+        /// 新增批号
+        /// </summary>
+        /// <param name="BcLotMaster"></param>
+        /// <returns></returns>
+        public bool Insert(BcLotMaster BcLotMaster)
+        {
+            return InsertOrUpdate(BcLotMaster);
+        }
+
+        /// <summary>
+        /// 获取批号信息
+        /// </summary>
+        /// <param name="LotCode"></param>
+        /// <returns></returns>
+        public BcLotMaster GetBcLotMaster(string LotCode)
+        {
+
+            return GetFirst(q => q.LotCode == LotCode);
+        }
+    }
+}
