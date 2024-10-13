57ee4a80a443da8e42689218dfa3f4ab1e457d07
黄卫东 <1668369668@qq.com>
2024-10-13 12:25:57 +0800

    提交

--- SQL/BarCode.sql
+++ SQL/BarCode.sql
@@ -1,3 +1,5 @@
+
+--批号主档表
 IF OBJECT_ID('QYD_BC_LotMaster') IS NOT NULL
 DROP TABLE QYD_BC_LotMaster
 GO
@@ -14,7 +16,46 @@ CREATE TABLE QYD_BC_LotMaster(
 	ValidDate int,--生效天数
 	InvalidDate DATETIME,--生效日期
 	SrcDocNo VARCHAR(50),--创建人
-	AutoCode BIGINT,--自动编码ID
+	AutoCode int,--是否自动编码
 	Memo VARCHAR(500)--备注
+)
+GO
+
+--批次规则表
+IF OBJECT_ID('QYD_BC_LotRule') IS NOT NULL
+DROP TABLE QYD_BC_LotRule
+GO
+
+CREATE TABLE QYD_BC_LotRule(
+	ID BIGINT PRIMARY KEY, --标识
+	SysVersion BIGINT DEFAULT 0, --版本
+	CreatedTime DATETIME DEFAULT GETDATE(),--创建时间
+	CreatedBy VARCHAR(50) DEFAULT '',--创建人
+	RuleCode VARCHAR(100),--规则编码
+	RuleName VARCHAR(100),--规则名称
+	Org BIGINT,--组织ID
+	RuleDes VARCHAR(500),--创建人
+	Memo VARCHAR(500)--备注
+)
+GO
+
+
+--批次规则明细表
+IF OBJECT_ID('QYD_BC_LotRuleLine') IS NOT NULL
+DROP TABLE QYD_BC_LotRuleLine
+GO
+
+CREATE TABLE QYD_BC_LotRuleLine(
+	ID BIGINT PRIMARY KEY, --标识
+	SysVersion BIGINT DEFAULT 0, --版本
+	CreatedTime DATETIME DEFAULT GETDATE(),--创建时间
+	CreatedBy VARCHAR(50) DEFAULT '',--创建人
+	LotRule BIGINT,--批次规则ID
+	DsType int,--数据源类型
+	DtFormat int,--数据源类型
+	FillInCode VARCHAR(50),--补位符
+	SplitCode VARCHAR(50),--分隔符
+	IsFlow BIT,--是否流水依据
+	Memo VARCHAR(500)--数据源类型
 )
 GO
