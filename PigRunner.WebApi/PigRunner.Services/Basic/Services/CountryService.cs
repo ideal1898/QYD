b0ea32113361d6a43b655684e22508206fbfc794
黄卫东 <1668369668@qq.com>
2024-10-30 17:54:36 +0800

    国家档案接口调整

--- PigRunner.WebApi/PigRunner.Services/Basic/Services/CountryService.cs
+++ PigRunner.WebApi/PigRunner.Services/Basic/Services/CountryService.cs
@@ -45,15 +45,30 @@ namespace PigRunner.Services.Basic.Services
                     if (string.IsNullOrEmpty(request.Code))
                         throw new Exception("编码不能为空！");
                     Country head = repository.GetFirst(q => q.Code == request.Code);
-                    if (head == null)
-                        head = Country.Create();
+
+                    if(request.OptType == 0)
+                    {
+                        if(head != null)
+                        throw new Exception(string.Format("编码为【{0}】的国家地区已存在，不能再新增！",request.Code));
+                        else
+                            head = Country.Create();
+                    }
+                    else
+                    {
+                        if (request.ID <= 0)
+                            throw new Exception("修改ID要大于零！");
+                        head = repository.GetFirst(q => q.ID == request.ID);
+                            if (head == null)
+                                throw new Exception(string.Format("ID为【{0}】的国家地区不存在，请检查！", request.ID));
+                    }
+                    
                     head.Code = request.Code;
                     head.Name = request.Name;
-                    head.CountryFormat = request.CountryFormat;
-                    head.Currency = request.Currency;
-                    head.Language = request.Language;
-                    head.NameFormat = request.NameFormat;
-                    head.TimeZone = request.TimeZone;
+                    head.CountryFormat = request.CountryFormatV;
+                    head.Currency = request.CurrencyV;
+                    head.Language = request.LanguageV;
+                    head.NameFormat = request.NameFormatV;
+                    head.TimeZone = request.TimeZoneV;
                     response.id = head.ID;
 
                     bool isSuccess = repository.InsertOrUpdate(head);
@@ -74,23 +89,31 @@ namespace PigRunner.Services.Basic.Services
                 }
                 else if (request.OptType == 3)
                 {
+                    List<CountryVo> list = new List<CountryVo>();
+
                     if (!string.IsNullOrEmpty(request.Code))
                     {
-                        Country head = repository.GetFirst(q => q.Code == request.Code);
-                        if (head == null)
+                        Country item = repository.GetFirst(q => q.Code == request.Code);
+                        if (item == null)
                             throw new Exception(string.Format("编码为【{0}】的国家地区不存在！", request.Code));
-
-                        List<Country> list = new List<Country>();
-                        list.Add(head);
-
-                        response.data = JArray.FromObject(list);
+                        CountryVo dto = SetValue(item);
+                        dto.LineNum = 10;
+                        list.Add(dto);
                     }
                     else
                     {
 
                         var lst = repository.Context.Queryable<Country>().ToList();
-                        response.data = JArray.FromObject(lst);
+                        int lineNum = 10;
+                        foreach (var item in lst)
+                        {
+                            CountryVo dto = SetValue(item);
+                            dto.LineNum = lineNum;
+                            list.Add(dto);
+                            lineNum += 10;
+                        }
                     }
+                    response.data = JArray.FromObject(list);
                 }
                 response.success = true;
                 response.code = 200;
@@ -101,6 +124,45 @@ namespace PigRunner.Services.Basic.Services
                 response.msg = ex.Message;
             }
             return response;
+        }
+
+        private CountryVo SetValue(Country item)
+        {
+            CountryVo dto = new CountryVo();
+            dto.OptType = 1;
+            dto.Code = item.Code;
+            dto.Name = item.Name;
+            dto.ID = item.ID;
+            dto.LanguageV = item.Language;
+            if (dto.LanguageV == 0)
+                dto.Language = "中文";
+            else if (dto.LanguageV == 1)
+                dto.Language = "外语";
+
+            dto.CountryFormatV = item.CountryFormat;
+            if (dto.CountryFormatV == 0)
+                dto.CountryFormat = "中国大陆";
+            else if (dto.CountryFormatV == 1)
+                dto.CountryFormat = "境外地址";
+
+            dto.CurrencyV = item.Currency;
+            if (dto.CurrencyV == 0)
+                dto.Currency = "人民币";
+            else if (dto.CurrencyV == 1)
+                dto.Currency = "外币";
+
+            dto.NameFormatV = item.NameFormat;
+            if (dto.NameFormatV == 0)
+                dto.NameFormat = "先姓后名";
+            else if (dto.NameFormatV == 1)
+                dto.NameFormat = "先名后姓";
+
+            dto.TimeZoneV = item.TimeZone;
+            if (dto.TimeZoneV == 0)
+                dto.TimeZone = "东八区";
+            else if (dto.TimeZoneV == 1)
+                dto.TimeZone = "UTC+8:00（北京、上海、重庆、乌鲁木齐）";
+            return dto;
         }
 
     }
